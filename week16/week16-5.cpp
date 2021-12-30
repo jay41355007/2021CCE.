703b0b648e2af2860c42a7d7066152821bd2dd82
黃天駿 <91250218+jay41355007@users.noreply.github.com>
2021-12-30 17:39:34 +0800

    Add files via upload

---
+++ week16/week16-5.cpp
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ #include <string.h>
+
+
+char line[300]="10010110100001";
+int main()
+{
+    int ans;
+    int N= strlen(line);
+    for(int i=0;i<N;i++){
+        if(line[1]='1')ans++;
+    }
+    printf("¦³%d­Ó1\n",ans);
+}
